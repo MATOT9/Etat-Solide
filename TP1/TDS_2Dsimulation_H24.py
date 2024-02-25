88c5f3419068c0f292ac06b47804384b4bf6a078
Alyx Olivier <alyx.olivier@gmail.com>
2024-02-25 05:21:49 -0500

    Bugfixes et refactoring

--- TP1/TDS_2Dsimulation_H24.py
+++ TP1/TDS_2Dsimulation_H24.py
@@ -21,30 +21,37 @@ from scipy import constants as cte
 
 # win = 500 # peut aider à définir la taille d'un autre objet visuel comme un histogramme proportionnellement à la taille du canevas.
 # Déclaration de variables influençant le temps d'exécution de la simulation
-Natoms = 200    # change this to have more or fewer atoms
-dt = 1e-5       # pas d'incrémentation temporel
+Natoms: int = 200    # change this to have more or fewer atoms
+dt: float = 1e-5       # pas d'incrémentation temporel
 
 # variable à ajouter Partie 1 num 3
-timeLoopLen = 1000  # temps de simulation
-analyseSphere = 106  # à changer si analyse autre atome He (int [0, 199])
-histPosSphereX = histPosSphereY = np.zeros(timeLoopLen) # va permettre d'enregistrer toutes les poistions pour obtenir la vitesse en composantes
-distanceCollision = []  # vecteur de distance parcourrue
-timeCollision = []  # vecteur de temps écoulé
+timeLoopLen: int = 1000  # temps de simulation
+analyseSphere: int = 106  # à changer si analyse autre atome He (int [0, 199])
+
+# NE JAMAIS FAIRE ÇA AVEC DES LISTE, LES DEUX LISTES SONT LE MÊME OBJET
+# histPosSphereX = histPosSphereY = np.zeros(timeLoopLen)
+histPosSphereX = np.zeros(timeLoopLen)
+histPosSphereY = np.zeros(timeLoopLen) # va permettre d'enregistrer toutes les positions pour obtenir la vitesse en composantes
+
+xCollision: list[float] = [] # Distances x et y parcourues
+yCollision: list[float] = []
+distanceCollision: list[float] = [] # Distance scalaire parourue
+timeCollision: list[float] = []  # vecteur de temps écoulé
 
 # Déclaration de variables physiques "Typical values"
-mass = cte.value("alpha particle mass")
-Ratom = 0.01
-T = 300
+mass: float = cte.value("alpha particle mass")
+Ratom: float = 0.01
+T: float = 300
 
 # CANEVAS DE FOND
-L = 1                       # container is a cube L on a side
+L: float = 1                       # container is a cube L on a side
 gray = vp.color.gray(0.7)   # color of edges of container and spheres below
 animation = vp.canvas(width=750, height=500)
 animation.range = L
 animation.title = 'Cinétique des gaz parfaits'
 
 # ARÊTES DE BOÎTE 2D
-d = L/2 + Ratom
+d: float = L/2 + Ratom
 cadre = vp.curve(color=gray, radius=0.005)
 cadre.append(
     [
@@ -57,17 +64,20 @@ cadre.append(
 )
 
 # POSITION ET QUANTITÉ DE MOUVEMENT INITIALE DES SPHÈRES
-Atoms = []  # Objet qui contiendra les sphères pour l'animation
-p = []      # quantité de mouvement des sphères
-apos = []   # position des sphères
+Atoms: list[vp.simple_sphere] = []  # Objet qui contiendra les sphères pour l'animation
+p: list[vp.vector] = []      # quantité de mouvement des sphères
+apos: list[vp.vector] = []   # position des sphères
 
 # Principe de l'équipartition de l'énergie en thermodynamique statistique classique
-pavg = (2 * mass * 1.5 * cte.k * T)**0.5
+# La bonne formule n'a pas de 1.5 vu qu'on est en 2D???
+# pavg: float = (2 * mass* 1.5 * cte.k * T)**0.5 anciennement
+pavg: float = (2 * mass * cte.k * T)**0.5
 
 for i in range(Natoms):
     # position aléatoire qui tient compte que l'origine est au centre de la boîte
-    xyz = L * np.random.rand(3) - L/2
-    xyz[-1] = pz = 0
+    xyz: np.ndarray[float] = L * np.random.rand(3) - L/2
+    xyz[-1]: float = 0
+    pz: float = 0
 
     # liste de la position initiale de toutes les sphères
     apos.append(vp.vec(*xyz))
@@ -80,9 +90,9 @@ for i in range(Natoms):
         sphere = simple_sphere(radius=Ratom, color=gray)
 
     Atoms.append(sphere)
-    phi = 2 * np.pi * random.random()  # direction aléatoire pour la quantité de mouvement
-    px = pavg * np.cos(phi)     # quantité de mvt initiale selon l'équipartition
-    py = pavg * np.sin(phi)
+    phi: float = 2 * np.pi * random.random()  # direction aléatoire pour la quantité de mouvement
+    px: float = pavg * np.cos(phi)     # quantité de mvt initiale selon l'équipartition
+    py: float = pavg * np.sin(phi)
 
     # liste de la quantité de mvt initiale de toutes les sphères
     p.append(vp.vec(px, py, pz))
@@ -90,36 +100,45 @@ for i in range(Natoms):
 
 # FONCTION POUR IDENTIFIER LES COLLISIONS, I.E. LORSQUE LA DISTANCE ENTRE LES CENTRES DE 2 SPHÈRES EST À LA LIMITE DE S'INTERPÉNÉTRER ####
 def checkCollisions():
-    hitlist = []
+    hitlist: list[list[int]] = []
 
     # distance critique où les 2 sphères entre en contact à la limite de leur rayon
-    r2 = (2 * Ratom)**2
+    r2: float = (2 * Ratom)**2
 
     for i in range(Natoms):
-        ai = apos[i]
+        ai: vp.vector = apos[i]
         for j in range(i) :
-            aj = apos[j]
+            aj: vp.vector = apos[j]
 
             # la boucle dans une boucle itère pour calculer cette distance
             # vectorielle dr entre chaque paire de sphère
-            dr = ai - aj
+            dr: vp.vector = ai - aj
             if vp.mag2(dr) < r2:
                 # liste numérotant toutes les paires de sphères en collision
                 hitlist.append([i,j])
     return hitlist
 
-def tracingParticule(sphere, List, count):
-    # fonction ajoutee pour track la particule
-    l1 = l2 = np.empty(len(List))
-    for i, _ in enumerate(l1):
-        l1[i] = List[i][0]
-        l2[i] = List[i][1]
+def trackingParticule(sphere: int, List: list[list[int]], count: int):
+    # Fonction ajoutée pour track la particule
+    # Liste de tous les index des sphères en collision
+    collisions: list[int] = []
+    [collisions.extend(elem) for elem in List]
 
+    # Ajoute le temps écoulé et la distance parcourue pendant le temps dt
+    # au total depuis la dernière collision
     timeCollision[-1] += dt
+    
     varX = histPosSphereX[count] - histPosSphereX[count - 1]
     varY = histPosSphereY[count] - histPosSphereY[count - 1]
-    distanceCollision[-1] += np.linalg.norm(varX + varY)
-    if sphere in l1 or sphere in l2:
+    xCollision[-1] += varX
+    yCollision[-1] += varY
+    distanceCollision[-1] += np.sqrt(varX**2+varY**2)
+
+    # Si la sphère subit une collision, passer à la prochaine valeur
+    # de temps et de distance entre collisions
+    if sphere in collisions:
+        xCollision.append(0)
+        yCollision.append(0)
         distanceCollision.append(0)
         timeCollision.append(0)
 
@@ -131,18 +150,19 @@ for count in range(timeLoopLen):
     vp.rate(300)
 
     # DÉPLACE TOUTES LES SPHÈRES D'UN PAS SPATIAL deltax
-    vitesse = []    # vitesse instantanée de chaque sphère
-    deltax = []     # pas de position des sphère (incrément de temps dt)
+    vitesse: list[vp.vector] = []    # vitesse instantanée de chaque sphère
+    deltax: list[vp.vector] = []     # pas de position des sphère (incrément de temps dt)
     for i in range(Natoms):
         vitesse.append(p[i] / mass)
         deltax.append(vitesse[i] * dt)
 
         # nouvelle position de l'atome après l'incrément de temps dt
-        Atoms[i].pos = apos[i] = apos[i] + deltax[i]
+        apos[i] += deltax[i]
+        Atoms[i].pos = apos[i]
 
         # CONSERVE LA QUANTITÉ DE MOUVEMENT AUX COLLISIONS AVEC LES PAROIS DE LA BOÎTE
     # for i in range(Natoms):
-        loc = apos[i]
+        loc: vp.vector = apos[i]
         if abs(loc.x) > L/2 and loc.x < 0:
             # renverse composante x à la paroi de gauche
             p[i].x = abs(p[i].x)
@@ -158,13 +178,13 @@ for count in range(timeLoopLen):
             p[i].y = -abs(p[i].y)
 
     # LET'S FIND THESE COLLISIONS!!!
-    hitlist = checkCollisions()
+    hitlist: list[list[int]] = checkCollisions()
     histPosSphereX[count] = apos[analyseSphere].x
     histPosSphereY[count] = apos[analyseSphere].y 
     if count != 0:
-        tracingParticule(analyseSphere, hitlist, count)
+        trackingParticule(analyseSphere, hitlist, count)
     else:
-        distanceCollision, timeCollision  = [0], [0]
+        xCollision, yCollision, distanceCollision, timeCollision = [0], [0], [0], [0]
 
     # CONSERVE LA QUANTITÉ DE MOUVEMENT AUX COLLISIONS ENTRE SPHÈRES
     for ij in hitlist:
@@ -184,8 +204,10 @@ for count in range(timeLoopLen):
         rrel = posi - posj
         vrel = vj - vi
 
-        # exclusion de cas où il n'y a pas de changements à faire
-        if vrel.mag2 == 0 or rrel.mag > Ratom:
+        # exclusion de cas où il n'y a pas de changements à faire, 2 cas:
+        # exactly same velocities si et seulement si le vecteur vrel devient nul, la trajectoire des 2 sphères continue alors côte à côte
+        # one atom went all the way through another, la collision a été "manquée" à l'intérieur du pas deltax
+        if vrel.mag2 == 0 or rrel.mag > 2*Ratom:
             continue
 
         # calcule la distance et temps d'interpénétration des sphères dures qui ne doit pas se produire dans ce modèle
@@ -204,8 +226,9 @@ for count in range(timeLoopLen):
         pcomj = p[j]-mass*Vcom
 
         # bounce in center-of-momentum (com) frame
-        pcomi = pcomi - 2 * vp.dot(pcomi, rrel) * rrel.hat
-        pcomj = pcomj - 2 * vp.dot(pcomj, rrel) * rrel.hat
+        rrel = vp.hat(rrel)
+        pcomi -= 2 * vp.dot(pcomi, rrel) * rrel
+        pcomj -= 2 * vp.dot(pcomj, rrel) * rrel
 
         # transform momenta back to lab frame
         p[i] = pcomi + mass * Vcom
