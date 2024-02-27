ca9d1198ecf8178854b9bbcd400e5d1249aaff83
Alyx Olivier <alyx.olivier@gmail.com>
2024-02-27 01:56:36 -0500

    Simulation de Drude terminé (s'il n'y a pas de bugs)

--- TP1/Particules.py
+++ TP1/Particules.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+from copy import deepcopy
 from functools import partial
 from scipy import constants as cte
+from scipy.stats import maxwell
 import numpy as np
 import random
 import vpython as vp
@@ -7,16 +10,18 @@ import vpython as vp
 
 
 class Particule:
-    def __init__(self, position: vp.vector, qteMvt: vp.vector, masse: float, rayon: float, charge: float=0, couleur: vp.color=vp.color.gray(0.7)):
+    def __init__(self, rayon: float, position: vp.vector, p: vp.vector = vp.vector(0,0,0),
+                 masse: float = 0, charge: float = 0, couleur: vp.color = None):
         self.pos: vp.vector = position
-        self.p: vp.vector = qteMvt
+        self.p: vp.vector = p
         self.masse: float = masse
         self.rayon: float = rayon
         self.charge: float = charge
-        self.sphere = vp.simple_sphere(pos=position, radius=rayon, color=couleur)
+        if couleur:
+            self.sphere = vp.simple_sphere(pos=position, radius=rayon, color=couleur)
 
 
-    def update(self, dt: float, L: float, E: float) -> None:
+    def update(self, dt: float, L: float, E: float = 0) -> None:
         # Le champ électrique accélère la particule
             # À faire
 
@@ -35,7 +40,8 @@ class Particule:
             self.p.y = -abs(self.p.y)
 
         # Update la position des sphères pour l'animation
-        self.sphere.pos = self.pos
+        if self.sphere:
+            self.sphere.pos = self.pos
 
 
     # FONCTION POUR IDENTIFIER LES COLLISIONS, I.E. LORSQUE LA DISTANCE ENTRE LES CENTRES DE 2 SPHÈRES EST À LA LIMITE DE S'INTERPÉNÉTRER ####
@@ -61,7 +67,7 @@ class Particule:
 
 
     @staticmethod
-    def collisionAtomes(atome1: Particule, atome2: Particule, T: float) -> None:
+    def collisionAtomes(atome1: Particule, atome2: Particule) -> None:
         # Positions et vitesses relatives des deux particules
         v1: vp.vector = atome1.p/atome1.masse
         v2: vp.vector = atome2.p/atome2.masse
@@ -74,21 +80,37 @@ class Particule:
         if vrel.mag2 == 0 or rrel.mag > atome1.rayon+atome2.rayon:
             return None
 
-        # Calcule la nouvelle quantité de mouvement de l'électron avec sa norme distribuée selon une distribution
-        # Maxwell-Boltzman avec sa direction aléatoire
-            # À implémenter
+        # Calcule la distance et temps d'interpénétration des sphères dures qui ne doit pas se produire dans ce modèle
+        dx: float = vp.dot(rrel, vrel.hat)
+        dy: float = vp.cross(rrel, vrel.hat).mag
+        # Alpha est l'angle du triangle composé de rrel, la trajectoire de l'atome 2 et la ligne
+        # entre le centre de l'atome 1 vers le centre de l'atome 2 où les deux atomes se touchent
+        alpha = vp.asin(dy / (atome1.rayon+atome2.rayon))
+        # Distance parcourue à l'intérieur du l'atome à partir du premier contact
+        d: float = (atome1.rayon+atome2.rayon) * vp.cos(alpha) - dx
+        # Temps écoulé pour se déplacer de la première collisions à la position à l'intérieur de l'atome
+        deltat: float = d / vrel.mag
 
-        # Calcule la distance à parcourir pour que l'électron quitte le noyau avec sa nouvelle trajectoire et avance l'électron
-        # pour ne pas qu'il interagisse avec le même coeur plusieurs fois d'affilée
-            # À implémenter
+        # Transform momenta to center-of-momentum (com) frame
+        Vcom: vp.vector = (atome1.p+atome2.p) / (atome1.masse+atome2.masse)
+        pcom1 = atome1.p - atome1.masse*Vcom
+        pcom2 = atome2.p - atome2.masse*Vcom
+
+        # Bounce in center-of-momentum (com) frame
+        rrel = vp.hat(rrel)
+        atome1.p -= 2 * vp.dot(pcom1, rrel) * rrel
+        atome2.p -= 2 * vp.dot(pcom2, rrel) * rrel
+
+        # Change l'interpénétration des sphères par la cinétique de collision,
+        # puis avance de deltat dans le temps, ramenant au même temps où sont
+        # rendues les autres sphères dans l'itération
+        atome1.pos += (atome1.p / atome1.masse - v1) * deltat
+        atome2.pos += (atome2.p / atome2.masse - v2) * deltat
 
 
     @staticmethod
-    def checkCollisionsCoeurs(electrons: list[Particule], coeurs: list[Particule]) -> list[list[int]]
+    def checkCollisionsElectrons(electrons: list[Particule], coeurs: list[Particule]) -> list[list[int]]:
         hitlist: list[int] = []
-
-        # distance critique où les 2 sphères entre en contact à la limite de leur rayon
-        r2: float = (atomes[0].rayon)**2
 
         for i in range(len(coeurs)) :
             coeur = coeurs[i]
@@ -96,7 +118,7 @@ class Particule:
             r: float = electrons[0].rayon+coeur.rayon
 
             for j in range(len(electrons)):
-            electron = electrons[j]
+                electron = electrons[j]
                 # La boucle dans une boucle itère pour calculer cette distance
                 # vectorielle dr entre électron et coeur
                 dr: vp.vector = electron.pos - coeur.pos
@@ -107,15 +129,35 @@ class Particule:
 
 
     @staticmethod
-    def collisionsElectrons(electron: Particule, coeur: Particule) -> None:
+    def collisionsElectrons(electron: Particule, coeur: Particule, T: float) -> None:
         # Positions relative de l'électron et du coeur
         rrel: vp.vector = electron.pos - coeur.pos
+        r: float = vp.mag(rrel)
+        R: float = electron.rayon+coeur.rayon
 
         # Exclusion de cas où il n'y a pas de changements à faire, 2 cas:
         # exactly same velocities si et seulement si p de l'électron devient nul, pas de collision
         # one atom went all the way through another, la collision a été "manquée" à l'intérieur du pas deltax
-        if vp.mag(electron.p) == 0 or rrel.mag > atome1.rayon+atome2.rayon:
+        if vp.mag(electron.p) == 0 or rrel.mag > R:
             return None
+
+        # Calcule la nouvelle quantité de mouvement de l'électron avec sa norme distribuée
+        # selon une distribution Maxwell-Boltzman avec sa direction aléatoire
+        scale: float = np.sqrt(electron.masse*cte.k*T)
+        p: float = maxwell.rvs(scale=scale)
+        phi: float = 2 * np.pi * random.random()
+        px: float = p*np.cos(phi)
+        py: float = p*np.sin(phi)
+        electron.p = vp.vector (px, py, 0)
+
+        # Calcule la distance à parcourir pour que l'électron quitte le noyau avec sa nouvelle trajectoire
+        # et avance l'électron pour ne pas qu'il interagisse avec le même coeur plusieurs fois d'affilée
+        # cos de l'angle entre -rrel et p
+        cosTheta: float = vp.dot(-rrel, electron.p)/(r*p)
+        # Distance à parcourir
+        d = r*cosTheta + np.sqrt(r**2*cosTheta**2 + R**2-r**2)
+        # On avance en direction de p d'une distance d
+        electron.pos += vp.vector(px, py, 0)*d/p
 
 
 def canvas(L: float, rParticule: float) -> None:
@@ -137,10 +179,10 @@ def canvas(L: float, rParticule: float) -> None:
     )
 
 
-def setupParticules(L: float, masse: float, rayon: float, nParticules: int, T: float, analyseSphere: int) -> list[Particule]:
+def setupParticules(L: float, masse: float, rayon: float, nParticules: int, T: float, analyseSphere: int, render: bool = True) -> list[Particule]:
     # POSITION ET QUANTITÉ DE MOUVEMENT INITIALE DES SPHÈRES
     # Liste qui contient les atomes
-    atoms: list[vp.simple_sphere] = []
+    atoms: list[Particule] = []
 
     # Principe de l'équipartition de l'énergie en thermodynamique statistique classique
     # La bonne formule n'a pas de 1.5 vu qu'on est en 2D???
@@ -159,13 +201,83 @@ def setupParticules(L: float, masse: float, rayon: float, nParticules: int, T: f
         py: float = pavg * np.sin(phi)
         pInit = vp.vector (px, py, 0)
 
-        # On ajoute un atome à la liste des atomes
-        particule = partial(Particule, posInit, pInit, masse, rayon)
-        if i == analyseSphere:
-            # Garde une sphère plus grosse et colorée parmis toutes les grises
-            atoms.append(particule(vp.color.magenta))
-            atoms[-1].sphere.radius *= 2
-        else: 
-            atoms.append(particule(vp.color.gray(0.7)))
+        # On ajoute un atome à la liste des atomes, si render=False, l'attribut sphere de l'atome n'est pas initialisé
+        particule = partial(Particule, rayon, posInit, p=pInit, masse=masse)
+        if render:
+            couleur: vp.color = vp.color.gray(0.7)
+        else:
+            couleur = None
+
+        atoms.append(particule(couleur=couleur))
+
+    if render:
+        atoms[analyseSphere].sphere.radius *= 2
+        atoms[analyseSphere].sphere.color = vp.color.magenta
 
     return atoms
+
+
+def placerMotif(motif: list[list[float, vp.vector]], pos: vp.vector, L, render: bool = True) -> int:
+    # Nombre de coeurs placés dans le motif
+    coeursPlaces: int = 0
+    coeurs: list[Particule] = []
+
+    for coeur in motif:
+        copie = deepcopy(coeur)
+        copie[1] += pos
+        # Ne place les coeurs que si ils sont dans la boite
+        if abs(copie[1].x) < L/2 and abs(copie[1].y) < L/2:
+            # Ne rend les sphères visibles que si render est vrai
+            if render:
+                couleur: vp.color = vp.color.red
+            else:
+                couleur = None
+
+            coeurs.append(Particule(*copie, couleur=couleur))
+            coeursPlaces += 1
+
+    return coeurs, coeursPlaces
+
+
+def setupCoeurs(maille: list[vp.vector], motif: list[list[float, vp.vector]], L: float, render: bool = True) -> list[Particule]:
+    # Place un premier motif au centre de la boite, place
+    # un coeur uniquement si le coeur est dans la boite
+    coeurs: list[Particule] = []
+    coeursMotif, places = placerMotif(motif, vp.vector(0, 0, 0), L, render)
+    coeurs.extend(coeursMotif)
+    
+    rayon: int = 0
+    coeursPlaces: int = 0
+    while True:
+        # Nombre de coeurs placés au cours de l'itération, quitte
+        # la boucle si aucun n'est placé durant l'itération en cours
+        coeursPlaces = 0
+        # On rempli couche par couche en partant du centre
+        rayon += 1
+
+        for n1 in range(-rayon, rayon+1):
+            centre: vp.vector = n1*maille[0] + rayon*maille[1]
+            coeursMotif, places = placerMotif(motif, centre, L, render)
+            coeurs.extend(coeursMotif)
+            coeursPlaces += places
+
+            centre: vp.vector = n1*maille[0] - rayon*maille[1]
+            coeursMotif, places = placerMotif(motif, centre, L, render)
+            coeurs.extend(coeursMotif)
+            coeursPlaces += places
+
+        for n2 in range(-rayon+1, rayon):
+            centre: vp.vector = rayon*maille[0] + n2*maille[1]
+            coeursMotif, places = placerMotif(motif, centre, L, render)
+            coeurs.extend(coeursMotif)
+            coeursPlaces += places
+
+            centre: vp.vector = -rayon*maille[0] + n2*maille[1]
+            coeursMotif, places = placerMotif(motif, centre, L, render)
+            coeurs.extend(coeursMotif)
+            coeursPlaces += places
+
+        if coeursPlaces == 0:
+            break
+
+    return coeurs
--- TP1/TDS_2Ddrude_H24.py
+++ TP1/TDS_2Ddrude_H24.py
@@ -1,3 +1,85 @@
+# Third-party libraries
+from scipy import constants as cte
+from Particules import *
+import numpy as np
+import vpython as vp
+
+
+def main(dt: float, timeLoopLen: int, analyseSphere: int, E: float = 0, render: bool = True) -> [list[Particule]]:
+    # Déclaration de variables physiques "Typical values"
+    mass: float = cte.m_e
+    rElec: float = 0.01
+    T: float = 300
+
+    # Canevas de fond et arêtes de boîte 2d
+    L: float = 1    # container is a cube L on a side
+    canvas (L, rElec)
+
+    # win = 500 # peut aider à définir la taille d'un autre objet visuel comme un histogramme proportionnellement à la taille du canevas.
+    # change this to have more or fewer atoms
+    nElec: int = 200
+    # Maille primitive du cristal
+    maillePrimitive: list[vp.vector] = [vp.vector(L/5, 0, 0), vp.vector(0, L/5, 0)]
+
+    particule1: list[float, vp.vector] = [0.05, vp.vector(0, 0, 0)]
+    particule2: list[float, vp.vector] = [0.02, vp.vector(L/10, L/10, 0)]
+    motif: list[list[float, vp.vector]] = [particule1, particule2]
+
+    # Initialisation des atomes
+    electrons: list[Particule] = setupParticules(L, mass, rElec, nElec, T, analyseSphere, render=render)
+    coeurs: list[Particule] = setupCoeurs(maillePrimitive, motif, L, render=render)
+
+    # BOUCLE PRINCIPALE POUR L'ÉVOLUTION TEMPORELLE DE PAS dt
+    for _ in range(timeLoopLen):
+        # limite la vitesse de calcul de la simulation pour que l'animation soit
+        # visible à l'oeil humain!
+        if render:
+            vp.rate(60)
+
+        [electron.update(dt, L, E) for electron in electrons]
+
+        # LET'S FIND THESE COLLISIONS!!!
+        hitlist: list[list[int]] = Particule.checkCollisionsElectrons(electrons, coeurs)
+
+        # Calcule le résultat des collisions et bouge les atomes
+        for i, j in hitlist:
+            Particule.collisionsElectrons(electrons[i], coeurs[j], T)
+    
+    return electrons
+
+
+if __name__ == "__main__":
+    dt: float = 1e-8         # pas d'incrémentation temporel
+    timeLoopLen: int = 1000  # temps de simulation
+    analyseSphere: int = 106 # à changer si analyse autre atome He (int [0, 199])
+    electrons = main(dt, timeLoopLen, analyseSphere)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 # Standard library
 import random
 from functools import partial
@@ -165,3 +247,4 @@ while True:
                 phi = 2 * np.pi * random.random()  # Nouvelle direction aléatoire
                 p[i].x = pavg * np.cos(phi)  # Nouvelle composante x
                 p[i].y = pavg * np.sin(phi)  # Nouvelle composante y
+"""
